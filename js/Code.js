58649297128d47f2ea66765532029d8faf4766b6
PedroHenriqueByjus <126911767+PedroHenriqueByjus@users.noreply.github.com>
2023-07-27 18:01:03 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "SIMBA";
+const accessCode2 = "SCAR";
+const accessCode3 = "HAKUNA MATATA";
+
+function clues() {
+    
+    fill("black")
+    textSize(15)
+    text("M B I A S", 100,50)
+    fill("black")
+    text("Dica: Foi acusado de matar seu pai!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("A C S R", 700,150)
+    fill("white")
+    text("Dica: Matou seu irmão!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T U H A T M T A A K N A", 100,250)
+    fill("white")
+    text("Dica: É uma frase do idioma suaíle, uma língua falada na África l, que significa sem problemas ou não se preocupe!", 100,270)
+
+}
